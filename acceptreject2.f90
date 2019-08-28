1932abfca32855ddc3576a48034922de2804d251
ANNAMERINFRANCS <annamerinfrancis@gmail.com>
2019-08-29 03:00:15 +0530

    sleepy as fuck

--- acceptreject2.f90
+++ acceptreject2.f90
@@ -1,20 +1,23 @@
  program acceptreject2
     implicit none
-    real::r,x,y,accept_int,exact
+    real*8::r,x,y,accept_int,exact
     integer::i,n,point
    write(*,*)"value of n"
    read(*,*)n
 open(unit=27, file='acceptreject2.dat')
 6 point=0
 do i=1,n
-    call random_number(r)
-    if(y .lt. 4/(1+x**2))then
+    call random_number(x)
+    call random_number(y)
+    y=4*y
+    if(y .lt. 4.0d0/(1+x**2))then
         point=point+1
     end if
 end do
-accept_int=1.0d0*0.5d0*(real(point)/real(n))
+accept_int=1.0d0*4.0d0*(real(point)/real(n))
 exact=4*atan(1.0d0)
-write(27,*)n,"",accept_int,"",abs(accept_int-exact)
+write(*,*) n, accept_int,abs(accept_int-exact)
+write(27,*)n,,accept_int,abs(accept_int-exact)
 n=n*10
 if (n .le. 100000000) goto 6
 end program acceptreject2
